22cad91c8052f88d4120eadcd5b6ee23220c97b1
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-17 01:52:23 -0700

    added card action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:card]
+  def card
+    render partial: "users/card", locals: { user: @user }
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
