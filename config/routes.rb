d2eff2a9bb01dd17af21a5a49a4a6b0d251754ac
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-17 01:35:43 -0700

    installed simple form and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  devise_for :users
+  root 'pages#home'
+
+  get "users/:id/card", to: "users#card", as: "user_card"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
