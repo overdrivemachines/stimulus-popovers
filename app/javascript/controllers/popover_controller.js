6fddf7b4663aba7c2c9c651a42726c9ffa012e89
get.dipen@gmail.com <get.dipen@gmail.com>
2023-03-17 01:34:42 -0700

    import popover

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -1,7 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
+import Popover from 'stimulus-popover'
 
 // Connects to data-controller="popover"
-export default class extends Controller {
+export default class extends Popover {
   connect() {
+    // call Popover's connect
+    super.connect();
+    console.log("Popover controller connected");
   }
 }
